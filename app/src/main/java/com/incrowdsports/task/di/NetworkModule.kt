a325fbc8e31ae289cc1eb81c94b1e3f9b1108261
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2023-01-15 18:31:12 +0000

    feat: including Hilt Dependency Injection and preparing Network layer

---
+++ app/src/main/java/com/incrowdsports/task/di/NetworkModule.kt
@@ -0,0 +1,64 @@
+package com.incrowdsports.task.di
+
+import com.google.gson.GsonBuilder
+import com.incrowdsports.task.data.FixtureService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = "https://feeds.incrowdsports.com/provider/opta/football/v1/"
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create(GsonBuilder().create())
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun bindFixtureService(retrofit: Retrofit): FixtureService {
+        return retrofit.create(FixtureService::class.java)
+    }
+}
